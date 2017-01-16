f2f5d211dfb60818e0c194e17c07b988bdbcda62
Jonathan Alorda <jonathan.alorda@gmail.com>
2017-01-16 15:30:25 -0600

    opacity animation for .hello now on page load.

--- alorda.js
+++ alorda.js
@@ -4,8 +4,30 @@ $("h1").click(function(){
     console.log("The paragraph was clicked.");
 });
 
-$(".hello").on("click", "h1", function() {
-    $(this).fadeOut(1000, function() {
-        
+$(".hello").click(function() {
+    $(this).animate({
+        opacity: 1.0,
+    }, 1000, function() {
+        // Animate complete
+    });
+    console.log(".hello was clicked.");
+    $(this).css("color", "red");
+});
+
+$("document").ready(function() {
+    $(".hello").animate({
+        opacity: 1.0,
+    }, 800, function() {
+        console.log("happened when page loaded.");
     });
 });
+
+// $( "#clickme" ).click(function() {
+//   $( "#book" ).animate({
+//     opacity: 0.25,
+//     left: "+=50",
+//     height: "toggle"
+//   }, 5000, function() {
+//     // Animation complete.
+//   });
+// });
