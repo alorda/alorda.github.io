71f5b3f0c85fb88914798b2b1dec38533c57aec2
Jonathan Alorda <jonathan.alorda@gmail.com>
2017-01-16 10:06:35 -0600

    jQuery working finally, not using $\(document\).ready, <script> tag at bottom of html body.

--- alorda.js
+++ alorda.js
@@ -1,1 +1,11 @@
 console.log("Hello!");
+
+$("h1").click(function(){
+    console.log("The paragraph was clicked.");
+});
+
+$(".hello").on("click", "h1", function() {
+    $(this).fadeOut(1000, function() {
+        
+    });
+});
